a1305ec6576067d5a40fb58191f1b69304fc43f9
Chris Grabham <chrisgrabham@outlook.com>
2024-02-13 11:16:35 +0000

    Add webhooks

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
+STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
 
 
 # Application definition
