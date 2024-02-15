276c7d61bc9562589aa531d0447d223dc1d819c9
Chris Grabham <chrisgrabham@outlook.com>
2024-02-15 14:12:20 +0000

    Configure Django to use AWS S3 for static and media files

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['localhost', 'boutique-ado-chris-31c5e7838bcb.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'boutique-ado-chris-31c5e7838bcb.herokuapp.com']
 
 # Stripe Payment system
 STRIPE_CURRENCY = 'usd'
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
 
     # Other
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -189,6 +190,24 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-chris-31c5e7838bcb'
+    AWS_S3_REGION_NAME = 'eu-west-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
--- env.template.py
+++ env.template.py
@@ -1,3 +1,7 @@
 import os
 
+os.environ.setdefault('DEBUG', '')
 os.environ.setdefault('SECRET_KEY', '')
+os.environ.setdefault('STRIPE_PUBLIC_KEY', '')
+os.environ.setdefault('STRIPE_SECRET_KEY', '')
+os.environ.setdefault('STRIPE_WH_SECRET', '')
