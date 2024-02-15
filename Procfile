3567f9ad404ed7fc8bb9a3167e951b3c74e6d16a
Chris Grabham <chrisgrabham@outlook.com>
2024-02-15 10:30:50 +0000

    Prepare for Heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn boutique-ado-chris.wsgi:application
--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+
 # Import environment letiables
 if os.path.exists("env.py"):
     import env  # noqa
@@ -30,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['boutique-ado-chris.herokuapp.com', 'localhost']
 
 # Stripe Payment system
 STRIPE_CURRENCY = 'usd'
@@ -130,12 +132,17 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',  # noqa
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
